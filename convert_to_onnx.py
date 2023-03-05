97ba5516ecde840adf51646f4cc2628e2a5819b3
amorygalili <amorygalili@gmail.com>
2023-03-05 04:41:29 +0000

    wip

--- convert_to_onnx.py
+++ convert_to_onnx.py
@@ -4,11 +4,19 @@ from inference_mnist_model import Net
 
 
 def main():
-  pytorch_model = Net()
-  pytorch_model.load_state_dict(torch.load('pytorch_model.pt'))
-  pytorch_model.eval()
-  dummy_input = torch.zeros(280 * 280 * 4)
-  torch.onnx.export(pytorch_model, dummy_input, 'onnx_model.onnx', verbose=True)
+
+    # Set the path of the .torchscript file
+  path = "charged_up_2023-02-27.torchscript"
+
+  # Load the .torchscript file
+  model = torch.jit.load(path)
+
+
+  # pytorch_model = Net()
+  # pytorch_model.load_state_dict(torch.load('charged_up_2023-02-27.torchscript'))
+  # pytorch_model.eval()
+  # dummy_input = torch.zeros(280 * 280 * 4)
+  # torch.onnx.export(pytorch_model, dummy_input, 'onnx_model.onnx', verbose=True)
 
 
 if __name__ == '__main__':
